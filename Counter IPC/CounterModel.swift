f421543f3b4b8ae8d4666bf3d159cf93b15fd997
Seth Corker <seth.corker@gmail.com>
2024-09-07 12:15:27 +0100

    add swift project

---
+++ Counter IPC/CounterModel.swift
@@ -0,0 +1,22 @@
+//
+//  CounterModel.swift
+//  Counter IPC
+//
+//  Created by Seth Corker on 07/09/2024.
+//
+
+import Foundation
+
+class CounterModel {
+    var value: Int {
+        return 0
+    }
+    
+    func increment() {
+        
+    }
+    
+    func decrement() {
+        
+    }
+}
