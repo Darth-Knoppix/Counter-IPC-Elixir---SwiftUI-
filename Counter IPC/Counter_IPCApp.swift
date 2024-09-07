e990c06db4d43a6b3f31afcc48a4bb30572e941e
Seth Corker <seth@zeroheight.com>
2024-09-07 10:58:06 +0100

    Initial Commit

---
+++ Counter IPC/Counter_IPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_IPCApp.swift
+//  Counter IPC
+//
+//  Created by Seth Corker on 07/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Counter_IPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
